11a1b39705a730370922a97c8acc127e824ee6ff
Eric Roos <ericroos13@gmail.com>
2020-04-08 21:16:40 -0500

    team flow

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :team_members
+  has_many :teams, through: :team_members
+
+
 end
