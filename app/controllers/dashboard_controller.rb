b5c1a03e7b7502ff1a934a5e5807e07f2af7a713
Eric Roos <ericroos13@gmail.com>
2020-04-13 16:52:26 -0500

    fix

---
+++ app/controllers/dashboard_controller.rb
@@ -0,0 +1,3 @@
+class DashboardController < ApplicationController
+  skip_before_action :authenticate_user!
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   end
   devise_for :users
 
-  root 'teams#index'
+  root 'dashboard#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
