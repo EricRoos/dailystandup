5fe6cb73bab85ff7ef4231724ed6f77572793a11
Eric Roos <ericroos13@gmail.com>
2020-05-13 20:15:45 -0500

    fix caching code

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -10,17 +10,11 @@ class TeamsController < ApplicationController
   # GET /teams/1
   # GET /teams/1.json
   def show
-    @team_members = @team.team_members.includes(:roles, :user)
-    @current_team_member = @team_members.detect { |t| t.user == current_user }
-
-    @standup_reports = StandupReport
+    Rails.logger.info '-------------------------- ACTION STARTS HERE -----------------------'
+    @standup_reports = Rails.cache.fetch([@team, 'team-feed']) do
+      StandupReport
         .where(team_member_id: @team.team_members)
         .order(created_at: :desc)
-
-    if !Rails.cache.exist?(@team)
-      @standup_reports = Rails.cache.fetch(@team) do
-        @standup_reports = @standup_reports.includes(:team_member, survey_responses: [ :survey_question ] )
-      end
     end
   end
 
