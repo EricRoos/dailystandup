12abbf2098e495eaf901c0f91278052444759e17
Eric Roos <ericroos13@gmail.com>
2020-05-16 00:57:44 -0500

    update key

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,7 +9,7 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
-  config.cache_store = :mem_cache_store, Rails.application.credentials.config[:memcached][:server]
+  config.cache_store = :mem_cache_store, Rails.application.credentials.config[:memcached][:servers]
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
