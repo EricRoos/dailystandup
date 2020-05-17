3ec96e50e946161de896517e55d1d5652c9da33a
Eric Roos <ericroos13@gmail.com>
2020-05-17 17:06:32 -0500

    pwa

--- app/javascript/app/index.js
+++ app/javascript/app/index.js
@@ -1,3 +1,11 @@
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register('/service-worker.js', { scope: './' })
+    .then(function(reg) {
+      console.log('[Companion]', 'Service worker registered!');
+      console.log(reg);
+    });
+}
+
 $(document).on("ajax:success", ".like-button-wrapper", function handleLikeSuccess(data, status, xhr){
   $.get($(this).data('refresh-url'));
 });
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'service_worker/service_worker'
+  get 'service_worker/manifest'
   resources :surveys
   resources :teams do
     resources :standup_reports
@@ -8,5 +10,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   root 'teams#index'
+  get '/service-worker.js' => "service_worker#service_worker"
+  get '/manifest.json' => "service_worker#manifest"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
