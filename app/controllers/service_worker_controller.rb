087678b9d910fb859d1e84dc63f8be8091dc888b
Eric Roos <ericroos13@gmail.com>
2020-05-17 17:40:22 -0500

    dont require auth for manifest file

--- app/controllers/service_worker_controller.rb
+++ app/controllers/service_worker_controller.rb
@@ -1,5 +1,6 @@
 class ServiceWorkerController < ApplicationController
   protect_from_forgery except: :service_worker
+  skip_before_action :authenticate_user!, only: :manifest
 
   def service_worker
   end
