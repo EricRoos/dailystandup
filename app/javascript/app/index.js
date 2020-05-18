89797baa01c6f95301a833bc95f66e5c1e366aeb
Eric Roos <ericroos13@gmail.com>
2020-05-17 20:05:03 -0500

    now?

--- app/javascript/app/index.js
+++ app/javascript/app/index.js
@@ -1,5 +1,5 @@
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register('/service-worker.js', { scope: './' })
+  navigator.serviceWorker.register('/service-worker.js', { scope: '/' })
     .then(function(reg) {
       console.log('[Companion]', 'Service worker registered!');
       console.log(reg);
