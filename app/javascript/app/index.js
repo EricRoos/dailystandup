d084c2abaff388a36d0e93cf951f690d2a51f091
Eric Roos <ericroos13@gmail.com>
2020-05-19 14:22:52 -0500

    k

--- app/javascript/app/index.js
+++ app/javascript/app/index.js
@@ -1,10 +1,14 @@
-if (navigator.serviceWorker) {
-  navigator.serviceWorker.register('/service-worker.js', { scope: '/' })
-    .then(function(reg) {
-      console.log('[Companion]', 'Service worker registered!');
-      console.log(reg);
-    });
-}
+window.addEventListener('load', function() {
+  if (navigator.serviceWorker) {
+    navigator.serviceWorker.register('/service-worker.js', { scope: './' })
+      .then(function(reg) {
+        console.log('[Companion]', 'Service worker registered!');
+        console.log(reg);
+      }).catch(function(err){
+        console.log('failed to register');
+      });
+  }
+});
 
 $(document).on("ajax:success", ".like-button-wrapper", function handleLikeSuccess(data, status, xhr){
   $.get($(this).data('refresh-url'));
